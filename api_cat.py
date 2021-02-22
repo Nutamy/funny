78182fcef5481df83fb61015d4d2c73b12ba90d0
Kozulina Nataliya <natasha.pechenka@gmail.com>
2021-02-22 21:54:48 +0600

    azsdxc

---
+++ api_cat.py
@@ -0,0 +1,12 @@
+[{"status":
+    {"verified":true,
+    "sentCount":1},
+"type":"cat",
+"deleted":false,
+"_id":"5d38bc970f1c57001592f152",
+"source":"user",
+"user":"5a9ac18c7478810ea6c06381",
+"text":"The pink substance inside a cat's nail is called the \"quick\" or the \"dermis\". When trimming your cat's nails, be sure to only cut the upper white part of the nail, not the quick.",
+"createdAt":"2019-07-24T20:16:23.558Z","updatedAt":"2020-08-23T20:20:01.611Z",
+"__v":0,
+"used":false},{"status":{"verified":true,"sentCount":1},"type":"cat","deleted":false,"_id":"591f98703b90f7150a19c162","__v":0,"text":"One un-neutered female cat can, in five years, be responsible for over 20,000 descendants. Female cats can have their first litter as young as six months and can have up to three litters each yearâwith five or six kittens in each litter.","source":"api","updatedAt":"2020-08-23T20:20:01.611Z","createdAt":"2018-01-04T01:10:54.673Z","used":false,"user":"5a9ac18c7478810ea6c06381"}]
--- main.py
+++ main.py
@@ -10,16 +10,22 @@ templates = Jinja2Templates(directory = 'templates')
 
 class RequestAPI:
     url = 'https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2'
+    url_dogs = 'https://dog.ceo/api/breeds/list/all'
 
     def get_all(self):
         result = requests.get(self.url).json()
         return result['text']
+    def get_dogs(self):
+        result = requests.get(self.url_dogs).json()
+        return result['akita'[0]]
 
 @app.get('/')
 def index(request: Request):
     my_req = RequestAPI()
     cat_fact = my_req.get_all()
+    dog = my_req.get_dogs()
     templates.TemplateResponse('index.html', {
         'request': request, 
-        'cat_fact': cat_fact
+        'cat_fact': cat_fact,
+        'dog': dog
     })
